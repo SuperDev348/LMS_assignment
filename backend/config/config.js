dd1ac818e901035c46de5ae3f28a9ad059b53b81
SuperDev348 <vladimir.danil25@mail.ru>
2021-11-11 10:03:43 -0800

    update config

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,5 @@
 module.exports= {
-  port: 443,
+  port: 8080,
   secretKey : 'secretKey',
   expiredAfter: 60 * 60 * 1000 *24
 };
--- frontend/src/config/site.config.js
+++ frontend/src/config/site.config.js
@@ -1,5 +1,5 @@
 export default {
-  apiUrl: 'http://5.39.219.229:443',
+  apiUrl: 'http://5.39.219.229:8080',
   domain: "5.39.219.229",
   // apiUrl: "http://localhost:8081",
   // domain: "localhost:3000",
