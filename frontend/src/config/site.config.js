1ce905c2f3b51462bce164f80cca5b5ce6eac2bc
SuperDev348 <vladimir.danil25@mail.ru>
2021-11-12 13:15:50 -0800

    fix

--- frontend/src/config/site.config.js
+++ frontend/src/config/site.config.js
@@ -1,6 +1,6 @@
 export default {
   apiUrl: "http://www.onlmp.com:8080",
-  domain: "www.onlmp.com",
+  domain: "onlmp.com",
   // apiUrl: "http://localhost:8080",
   // domain: "localhost:3000",
 };
