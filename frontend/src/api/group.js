d1cdaf21c1995ac3295ebbb7de53ce93995be488
SuperDev348 <vladimir.danil25@mail.ru>
2024-03-16 16:35:41 +0100

    update group

--- frontend/src/api/group.js
+++ frontend/src/api/group.js
@@ -18,15 +18,6 @@ async function update(data) {
   }
 }
 
-async function remove(id) {
-  try {
-    await apiDelete(`/api/group/${id}`);
-    return Promise.resolve({message: 'success'})
-  } catch(error) {
-    return Promise.reject(error)
-  }
-}
-
 async function get(id) {
   try {
     const res = await apiGet(`/api/group/${id}`);
