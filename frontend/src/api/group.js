c84ce7a594676f351e210b954d072b0eea3efffb
SuperDev348 <vladimir.danil25@mail.ru>
2024-03-19 16:34:03 +0100

    fix

--- frontend/src/api/group.js
+++ frontend/src/api/group.js
@@ -9,6 +9,15 @@ async function create(data) {
   }
 }
 
+async function update(data) {
+  try {
+    await apiPut(`/api/group/${data._id}`, data);
+    return Promise.resolve({message: 'success'})
+  } catch(error) {
+    return Promise.reject(error)
+  }
+}
+
 async function remove(id) {
   try {
     await apiDelete(`/api/group/${id}`);
